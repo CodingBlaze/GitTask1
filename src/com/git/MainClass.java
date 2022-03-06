ae9c5e6413ea250a54b053f4767ab733407efd42
Vikash Kumar4 <vikash_kumar4@epam.com>
2022-03-06 16:06:10 +0530

    Added student objects

--- src/com/git/MainClass.java
+++ src/com/git/MainClass.java
@@ -3,8 +3,17 @@ package com.git;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		// creating student objects 
+		Student Vikash = new Student("Vikash",22,101);
+		
+		Student Uday = new Student("Uday",22,102);
+		
+		Student Sailesh = new Student("Sailesh",21,103);
+		
+		Student Suresh = new Student("Suresh",21,104);
+		
+		System.out.println(Vikash.getDetails());
 	}
 
 }

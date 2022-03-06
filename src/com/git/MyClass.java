fb6795adbea3ca20e2e202197ea3c62951cf4d16
Suresh <Suresh_Yarava@epam.com>
2022-03-06 14:58:49 +0530

    Adding student class

--- src/com/git/MyClass.java
+++ src/com/git/MyClass.java
@@ -1,10 +1,32 @@
 package com.git;
 
-public class MyClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Student{
+	// Declaring Student_name , Student_age,Student_roll_number
+	String Student_name;
+	int Student_age;
+	long Student_roll_number;
+	
+	//Initializing Instance variables using explicit constructor
+	Student(String Student_name,int Student_age,long Student_roll_number)
+	{
+		this.Student_name=Student_name;
+		this.Student_age=Student_age;
+		this.Student_roll_number=Student_roll_number;
 	}
-
+	
+	//Method to update Student_name
+	void Update_name(String New_name){
+		Student_name=New_name;
+	}
+	
+	//Method to update Student_age
+	void Update_age(int New_age) {
+		Student_age=New_age;
+	}
+	
+	//Returns the Student_name,Student_age and Student_roll_number as a String format. 
+	String To_String(){
+		return "Name: "+Student_name+",Age: "+Integer.toString(Student_age)+",Roll: "+Long.toString(Student_roll_number);
+	}
+	
 }

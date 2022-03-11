1f97956ce71befb79b7fe8dc3c2bd39eca7f5799
Suresh Yarava <suresh_yarava@epam.com>
2022-03-11 19:02:53 +0530

    Committed all the specified changes

--- src/com/git/Student.java
+++ src/com/git/Student.java
@@ -1,32 +1,33 @@
 package com.git;
 
-class Student{
+class Student {
 	// Declaring Student_name , Student_age,Student_roll_number
-	String Student_name;
-	int Student_age;
-	long Student_roll_number;
-	
-	//Initializing Instance variables using explicit constructor
-	Student(String Student_name,int Student_age,long Student_roll_number)
-	{
-		this.Student_name=Student_name;
-		this.Student_age=Student_age;
-		this.Student_roll_number=Student_roll_number;
+	String studentName;
+	int studentAge;
+	long studentRollNumber;
+
+	// Initializing Instance variables using explicit constructor
+	Student(String studentName, int studentAge, long studentRollNumber) {
+		this.studentName = studentName;
+		this.studentAge = studentAge;
+		this.studentRollNumber = studentRollNumber;
 	}
 
-	//Method to update Student_name
-	void updateName(String New_name){
-		Student_name=New_name;
+	// Method to update Student_name
+	void updateName(String newName) {
+		studentName = newName;
 	}
 
-	//Method to update Student_age
-	void updateAge(int New_age) {
-		Student_age=New_age;
+	// Method to update Student_age
+	void updateAge(int newAge) {
+		studentAge = newAge;
 	}
 
-	//Returns the Student_name,Student_age and Student_roll_number as a String format.
-	String getDetails(){
-		return "Name: "+Student_name+",Age: "+Integer.toString(Student_age)+",Roll: "+Long.toString(Student_roll_number);
+	// Returns the Student_name,Student_age and Student_roll_number as a
+	// String format.
+	String getDetails() {
+		return "Name: " + studentName + ",Age: " + Integer.toString(studentAge) + ",Roll: "
+				+ Long.toString(studentRollNumber);
 	}
-	
+
 }

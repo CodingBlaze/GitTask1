fa9609d8c3921a160ec84f9f72ec27ffc290e3a9
Suresh Yarava <suresh_yarava@epam.com>
2022-03-14 12:31:00 +0530

    Revert "Committed all the specified changes"
    This reverts commit 1f97956ce71befb79b7fe8dc3c2bd39eca7f5799.

--- src/com/git/Department.java
+++ src/com/git/Department.java
@@ -3,38 +3,37 @@ package com.git;
 import java.util.LinkedList;
 import java.util.List;
 
-class Department {
-	// Declaring Department_Name, Department_Dean, Department_Students
-	String departmentName;
-	String departmentDean;
-	List<Student> departmentStudents = new LinkedList<Student>();
+class Department{
+    // Declaring Department_Name, Department_Dean, Department_Students
+    String Department_Name;
+    String Department_Dean;
+    List<Student> Department_Students=new LinkedList<Student>();
 
-	// Initializing Instance variables using explicit constructor
-	Department(String departmentName, String departmentDean) {
-		this.departmentName = departmentName;
-		this.departmentDean = departmentDean;
-	}
+    //Initializing Instance variables using explicit constructor
+    Department(String Department_Name,String Department_Dean)
+    {
+        this.Department_Name=Department_Name;
+        this.Department_Dean=Department_Dean;
+    }
+    //Method to update Department_name
+    void updateName(String New_name){
+        Department_Name=New_name;
+    }
 
-	// Method to update Department_name
-	void updateName(String newName) {
-		departmentName = newName;
-	}
+    //Method to update Department_Dean
+    void updateDean(String New_Dean) {
+        Department_Dean=New_Dean;
+    }
+    //Method to add Students to Department_Students List
+    void addStudent(Student student_Object) {
+        Department_Students.add(student_Object);
+    }
 
-	// Method to update Department_Dean
-	void updateDean(String newDean) {
-		departmentDean = newDean;
-	}
-
-	// Method to add Students to Department_Students List
-	void addStudent(Student studentObject) {
-		departmentStudents.add(studentObject);
-	}
-
-	// Prints the details of Students within this Department.
-	void getStudentDetails() {
-		for (Student student_Object : departmentStudents) {
-			System.out.println(student_Object.getDetails());
-		}
-	}
+    //Prints the details of Students within this Department.
+    void getStudentDetails(){
+        for(Student student_Object:Department_Students) {
+        	System.out.println(student_Object.getDetails());
+        }
+    }
 
 }
--- src/com/git/MainClass.java
+++ src/com/git/MainClass.java
@@ -3,37 +3,39 @@ package com.git;
 public class MainClass {
 
 	public static void main(String[] args) {
-
-		// creating student objects
-		Student Vikash = new Student("Vikash", 22, 101);
-
-		Student Uday = new Student("Uday", 22, 102);
-
-		Student Sailesh = new Student("Sailesh", 21, 103);
-
-		Student Suresh = new Student("Suresh", 21, 104);
-
-		Student Ajay = new Student("Ajay", 22, 105);
-
+		
+		// creating student objects 
+		Student Vikash = new Student("Vikash",22,101);
+		
+		Student Uday = new Student("Uday",22,102);
+		
+		Student Sailesh = new Student("Sailesh",21,103);
+		
+		Student Suresh = new Student("Suresh",21,104);
+		
+		Student Ajay = new Student("Ajay",22,105);
+		
+		System.out.println(Vikash.getDetails());
+		
 		// creating department objects
-		Department Physics = new Department("Physics", "Suresh Babu");
-
-		Department History = new Department("History", "Vinay Gopal");
-
+		Department Physics = new Department("CSE","Suresh Babu");
+		
+		Department History = new Department("ECE","Vinay Gopal");
+		
 		// adding students to departments
 		Physics.addStudent(Vikash);
-
+		
 		Physics.addStudent(Uday);
-
-		History.addStudent(Suresh);
-
+		
+		Physics.addStudent(Suresh);
+		
 		History.addStudent(Sailesh);
-
+		
 		History.addStudent(Ajay);
-
-		History.addStudent(Ajay);
-
+		
+		History.getStudentDetails();
+		
 		Physics.getStudentDetails();
-	}
+		}
 
 }
